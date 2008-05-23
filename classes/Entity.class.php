1d8ec45b46530a10ea8f08f788a931b00eb13236
Flavio Garcia <piraz@candango.org>
2008-05-23 18:10:32 +0000

    Adding entity class

---
+++ classes/Entity.class.php
@@ -0,0 +1,79 @@
+<?php
+class Entity {
+    
+    /**
+     * Entity name
+     *
+     * @var string
+     */
+    private $name;
+    
+    /**
+     * Entity value
+     *
+     * @var string
+     */
+    private $value;
+    
+    /**
+     * Entity items
+     *
+     * @var array of entities
+     */
+    private $items;
+    
+    /**
+     * Return entity name
+     *
+     * @return string
+     */
+    public function getName() {
+        return $this->name;
+    }
+    
+    /**
+     * Set entity name
+     *
+     * @param string $name
+     */
+    public function setName( $name ) {
+        $this->name = $name;
+    }
+    
+    /**
+     * Return entity value
+     *
+     * @return string
+     */
+    public function getValue() {
+        return $this->value;
+    }
+    
+    /**
+     * Set entity value
+     *
+     * @param string $value
+     */
+    public function setValue( $value ) {
+        $this->value = $value;
+    }
+    
+    /**
+     * Return all entity items
+     *
+     * @return array of items
+     */
+    public function getItems() {
+        return $this->items;
+    }
+    
+    /**
+     * Set all entity items
+     *
+     * @param array $items
+     */
+    public function setItems( $items ) {
+        $this->items = $items;
+    }
+    
+}
