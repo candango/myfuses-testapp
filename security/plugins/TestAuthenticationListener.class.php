a1e790e21cc2bf5db5b2bb4c84f84652f1c5417e
Flavio Garcia <piraz@candango.org>
2009-03-25 14:08:45 +0000

    Adding listner to security application.

---
+++ security/plugins/TestAuthenticationListener.class.php
@@ -0,0 +1,12 @@
+<?php
+class TestAuthenticationListener implements MyFusesAuthenticationListener {
+    
+    public function authenticate( MyFusesSecurityManager $manager ) {
+        
+    }
+    
+    public function authenticationPerformed( MyFusesSecurityManager $manager ) {
+        
+    }
+    
+}
