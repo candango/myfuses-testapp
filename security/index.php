145faacb6ee2284040197439feb75385c54a11f3
Flavio Garcia <piraz@candango.org>
2009-03-25 14:07:42 +0000

    Cleaning code.

--- security/index.php
+++ security/index.php
@@ -1,17 +1,4 @@
 <?php
-/*function microtime_float() {
-    list($usec, $sec) = explode(" ", microtime());
-    return ((float)$usec + (float)$sec);
-}
-
-function get_mem_usage() {
-    return ( round( memory_get_usage() / 1024 ) ) . " Kb";
-}
-
-$time = microtime_float();
-
-var_dump( get_mem_usage() );*/
-
 require_once 'myfuses/MyFuses.class.php';
 
 // creating new iflux instance
@@ -20,7 +7,3 @@ $myFuses = MyFuses::getInstance();
 $myFuses->createApplication( "SecurityTestApp" );
 
 $myFuses->doProcess();
-/*
-var_dump( get_mem_usage() );
-
-var_dump( microtime_float() - $time );*/
