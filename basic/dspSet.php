9383c0296768234cec7e76392066b924f46adcea
Flavio Garcia <piraz@candango.org>
2017-10-22 22:32:58 -0400

    Added test with evaluate parameter as true.

--- basic/dspSet.php
+++ basic/dspSet.php
@@ -2,5 +2,5 @@
 
 <b>Value from the variable:</b> <?=$avariable?><br>
 
-<b>Value from the array index:</b> <?=$anarray['index']?>
-
+<b>Value from the array index:</b> <?=$anarray['index']?><br>
+<b>Value from the array index1:</b> <?=$anarray['index1']?>
