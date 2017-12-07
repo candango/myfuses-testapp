08f0213de92b1014fdacb36b607777b9b2272729
Flavio Garcia <piraz@candango.org>
2017-12-06 21:32:53 -0500

    Changed the project layout to bootstrap 4.

--- main/dspFooter.php
+++ main/dspFooter.php
@@ -1,1 +1,16 @@
-<h5>MyFuses PHP by Candango Opensource Group</h5>
+    </div>
+    <footer class="footer text-center">
+        <div class="container">
+            <span class="text-muted">MyFuses Framework by Candango Open Source Group</span>
+        </div>
+    </footer>
+
+    <!-- Bootstrap core JavaScript
+        ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="../../../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="static/node_modules/popper.js/dist/umd/popper.min.js"></script>
+    <script src="static/node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+</body>
+</html>
--- main/dspHeader.php
+++ main/dspHeader.php
@@ -1,3 +1,48 @@
-<h1><?=myexp("MYFUSES_TEST_APPLICATION")?></h1>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../../../favicon.ico">
 
-<p><?=$breadCrumb?></p>
+    <title>MyFuses Test App</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="static/node_modules/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="static/css/myfuses-testapp.css" rel="stylesheet">
+
+    <script src="static/node_modules/steal/steal.js" main="app"></script>
+
+</head>
+
+<body>
+
+    <header>
+        <!-- Fixed navbar -->
+        <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+            <a class="navbar-brand" href="#"><?=myexp("MYFUSES_TEST_APP")?></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" href="#">Disabled</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+
+    <div class="container-fluid">
+    <p><?=$breadCrumb?></p>
